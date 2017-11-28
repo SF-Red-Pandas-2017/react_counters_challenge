e55c271fe284164dac44288c0b5b438cd6d08f16
Elise & Karlie & Zach Broaddus <pair+empoplawski.karlieromas.zachbroaddus@devbootcamp.com>
2017-11-27 18:04:27 -0600

    got to release 3/4 ish and hit a wall

--- src/App.js
+++ src/App.js
@@ -3,15 +3,38 @@ import Counter from './Counter';
 import './App.css';
 
 class App extends React.Component {
+    constructor(props) {
+    super(props);
+    this.increment = this.increment.bind(this)
+    this.state = {
+      count1: 0,
+      count2: 0,
+      count3: 0
+    };
+  }
+
+  incrementCounters = [1,2,3]
+
+  increment(param, count) {
+    console.log(this.state["count" + param])
+    this.setState({ count: this.state["count" + param]})
+    console.log(count)
+  };
+
+  // decrement(props) {
+  //   this.setState({ count: this.state.count - props })
+  // };
+
   render() {
     return (
       <div className="page-center-frame">
-        <Counter myProps={1} />
-        <Counter myProps={2} />
-        <Counter myProps={3} />
+        <Counter incrementValue={1} increment={this.increment} count={this.state[`count${value}`]}/>
+        <Counter incrementValue={2} increment={this.increment} count={this.state.count2}/>
+        <Counter incrementValue={3} increment={this.increment} count={this.state.count3}/>
       </div>
     );
   }
 }
 
 export default App;
+
--- src/Counter.js
+++ src/Counter.js
@@ -4,31 +4,36 @@ import './Counter.css';
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0
-    };
+    // this.increment = this.props.increment.bind(this, this.props.incrementValue)
+    // this.state = {
+    //   count: 0
+    // };
+}
+  //   this.increment = this.increment.bind(this)
+  //   this.decrement = this.decrement.bind(this)
+  // }
 
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-  }
+  // increment(param) {
+  //   this.setState({ count: this.state.count + param })
+  // }
 
-  increment() {
-    this.setState({ count: this.state.count + this.props.myProps})
-  }
-
-  decrement() {
-    this.setState({ count: this.state.count - this.props.myProps })
-  }
+  // decrement() {
+  //   this.setState({ count: this.state.count - this.props.myProps })
+  // }
 
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+</button>
-        <div className="count">{this.state.count}</div>
-        <button onClick={this.decrement}>-</button>
+        <button onClick={() => this.props.increment(this.props.incrementValue, this.props.count)}>+</button>
+        <div className="count">{this.props.count}</div>
+        // <button onClick={this.decrement}>-</button>
+
       </div>
     );
   }
 }
 
 export default Counter
+
+
+// <MyComponent onClick={() => this.increment(someArgument, someOtherArgument)} />
